82ccf4c993e0c7f8c92efb108e05466ab5316d5f
minhwuan1234 <100684479+minhwuan1234@users.noreply.github.com>
2026-06-05 16:14:06 +0700

    Update config.js

--- tools/bd-mkt-daily/config.js
+++ tools/bd-mkt-daily/config.js
@@ -13,6 +13,8 @@ window.TOOL_REGISTRY.push({
     "ou_1f71198623d1dc71688fe1312390f7ee": "Nga Linh",
     "ou_d7d124081bfa6eabfb12e85166eca85f": "Giang",
     "ou_db7bca8d6a07437aaab422849ddc2c69": "Chi",
+    "ou_6993f5104b93fe3d774304bc9637884d": "Linh",
+    "ou_12548715eba533527311e76207c95ce4": "Minh Anh"
   },
 
   /* ══════════════════════════════
